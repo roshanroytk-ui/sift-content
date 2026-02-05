911b7a4fd848d087742391d034498660cf5060d4
roshanroytk-ui <roshanroytk@gmail.com>
2026-02-05 22:40:09 +0530

    Update generate_content.py

--- generate_content.py
+++ generate_content.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 import os
 API_KEY = os.environ["GNEWS_API_KEY"]
-URL = f"https://gnews.io/api/v4/top-headlines?lang=en&max=10&token={API_KEY}"
+URL = f"https://gnews.io/api/v4/top-headlines?lang=en&max=50&token={API_KEY}"
 
 response = requests.get(URL)
 data = response.json()
